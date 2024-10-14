7a4bf6665b0cc0f7d10877a731fa48196d8579ce
Fatima Adem Garcia <fademgarcia13@gmail.com>
2024-10-14 22:46:45 +0200

    Update happiness_14_10_24.py

--- happiness_14_10_24.py
+++ happiness_14_10_24.py
@@ -25,7 +25,7 @@ st.markdown(
 logo_WHR = Image.open('WHR.gif')
 image_Panneau= Image.open('photo test.gif')
 Logo_internet = Image.open('Internet.jpeg')
-bonheur= Image.open('bonheur.gif')
+#bonheur= Image.open('bonheur.gif')
 bandeau= Image.open('bandeau.jpeg')
 
 #2 - Daframe
